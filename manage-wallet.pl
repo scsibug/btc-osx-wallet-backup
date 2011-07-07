e6bace8a5de61757b2953210a78eb388df5e0a27
Greg Heartsfield <scsibug@imap.cc>
2011-07-07 01:06:36 -0500

    Misc cleanup, improvements to show.

--- manage-wallet.pl
+++ manage-wallet.pl
@@ -34,17 +34,25 @@ if ($num_args == 1 && $ARGV[0] eq "show") {
     chomp($current_alias);
     if ($? == 0) {
         # alias was found
-        print "Current wallet in use is \"$current_alias\"\n";
+        print "Current wallet in use is \"$current_alias\".\n";
     }
-    
+    if (-e "${BACKUP_GIT_REPO}/${current_alias}.dat") {
+        my $wallets_match = &do_files_match($ACTIVE_WALLET_LOCATION, "${BACKUP_GIT_REPO}/${current_alias}.dat");
+        if ($wallets_match) {
+            print "\tNo differences between active and backup wallets.\n";
+        } else {
+            print "\tActive wallet has outstanding changes NOT in backup.\n";
+        }
+    } else {
+        print "Error: alias \"${current_alias}\" does not exist in the backup repository.\n";
+        exit(1);
+    }
 } elsif ($num_args == 1 && $ARGV[0] eq "backup") {
     print "Backing up current wallet from $ACTIVE_WALLET_LOCATION\n";
     # find the alias for the current wallet
     $current_alias = `xattr -p btc-wallet-alias '$ACTIVE_WALLET_LOCATION'`;
     chomp($current_alias);
     if ($? == 0) {
-        # alias was found
-        print "Current wallet in use is \"$current_alias\"\n";
         # don't do any work if the md5 hashes of both wallets match.
         my $wallets_match = &do_files_match($ACTIVE_WALLET_LOCATION, "${BACKUP_GIT_REPO}/${current_alias}.dat");
         if ($wallets_match) {
@@ -58,9 +66,7 @@ if ($num_args == 1 && $ARGV[0] eq "show") {
         # save alias as an attribute on the wallet
         `xattr -w btc-wallet-alias $current_alias $ACTIVE_WALLET_LOCATION`;
     } 
-
-    print "wallet alias is $current_alias end\n";
-    print "Saving wallet with alias $current_alias\n";
+    print "Saving wallet with alias \"$current_alias\"\n";
     # copy from active wallet to repo
     print "copying from $ACTIVE_WALLET_LOCATION to ${BACKUP_GIT_REPO}/${current_alias}.dat\n";
     copy($ACTIVE_WALLET_LOCATION, "${BACKUP_GIT_REPO}/${current_alias}.dat")  or die "Copy failed: $!";
@@ -72,11 +78,16 @@ if ($num_args == 1 && $ARGV[0] eq "show") {
     my $desired_alias = $ARGV[1];
     print "activating wallet $ARGV[1] to $ACTIVE_WALLET_LOCATION\n";
     # TODO: verify desired alias exists in repo
-    # TODO: make sure that the current wallet is backed up (md5 sum of active and backup wallet match) before overwriting
-    # copy wallet with desired alias to the active wallet location
-    copy("${BACKUP_GIT_REPO}/${desired_alias}.dat", $ACTIVE_WALLET_LOCATION)  or die "Copy failed: $!";
-    # set file attribute with alias so we can recognize which wallet it is for other commands
-    `xattr -w btc-wallet-alias '$desired_alias' '$ACTIVE_WALLET_LOCATION'`;
+    if (-e "${BACKUP_GIT_REPO}/${desired_alias}.dat") {
+        # TODO: make sure that the current wallet is backed up (md5 sum of active and backup wallet match) before overwriting
+        # copy wallet with desired alias to the active wallet location
+        copy("${BACKUP_GIT_REPO}/${desired_alias}.dat", $ACTIVE_WALLET_LOCATION)  or die "Copy failed: $!";
+        # set file attribute with alias so we can recognize which wallet it is for other commands
+        `xattr -w btc-wallet-alias '$desired_alias' '$ACTIVE_WALLET_LOCATION'`;
+    } else {
+        print "Alias \"${desired_alias}\" does not exist in the backup repository.\n";
+        exit(1);
+    }
 } else {
     print $USAGE;
 }
